a24a3a3f7a94d85c17e59360204761884c95c8d3
K3therLOL <g3roylol@gmail.com>
2025-02-26 04:54:08 +0300

    Delete 1-Bash-exploring directory

--- 1-Bash-exploring/scripts/1-automation/01-folder-permission.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Задача:
-# Разработать сценарий, который создаёт каталог /tmp/shared и устанавливает на него права доступа rwxrwxrwx.
-
-DIR=/tmp/shared
-MODE=777
-mkdir $DIR
-chmod $MODE $DIR
