ca1da909f5fa31525a35d227b283b4505ad6026d
K3therLOL <g3roylol@gmail.com>
2025-11-06 13:34:26 +0300

    fixed nonsense msg

--- 4-process-management/01-real-effective-processes.sh
+++ 4-process-management/01-real-effective-processes.sh
@@ -4,5 +4,5 @@
 # 1. Найти в системе все процессы, у которых различаются реальные и эффективные пользовательские идентификаторы.
 # 2. Для обнаруженных процессов вывести имя программы.
 
-echo "Processes with setuid bit:"
+echo "Processes with difference in ruid and euid:"
 ps -eo ruser,user,comm | tail -n +2 | awk '{ if ($1 != $2) print $3}'
